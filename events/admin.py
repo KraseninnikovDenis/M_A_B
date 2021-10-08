c859c8df4683db89e82b7733c8922f5e5e5315a4
Денис <Krasheninnikov.0792@gmail.com>
2021-10-08 21:08:23 +0300

    2021-10-08_21-07

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import EventsDB
+
+admin.site.register(EventsDB)
--- events/models.py
+++ events/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
 class EventsDB  (models.Model):
-    event_image= models.ImageField(upload_to='image_event/')
-    event_text=models.CharField(max_length=100)
+    event_image= models.ImageField(upload_to='image_event/', verbose_name='картинка')
+    event_text=models.CharField(max_length=100, verbose_name= 'описание')
+
+    class Meta:
+        verbose_name_plural='Ивенты'
+        verbose_name='Ивент'
