91a40b58ec269272c33bff5caf1dbc2b64c9c6f0
Денис <Krasheninnikov.0792@gmail.com>
2021-10-11 20:54:56 +0300

    New DB

--- my_awesome_blog/settings.py
+++ my_awesome_blog/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'my_awesome_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'm_a_b',
+        'USER': 'postgres',
+        'PASSWORD':'11235813Krosh11235813',
+        'HOST':'localhost',
+        'PORT' : '5432',
     }
 }
 
