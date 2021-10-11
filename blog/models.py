8b6a6862a88fc8c38f68da2d791c2d75b4fda610
Денис <Krasheninnikov.0792@gmail.com>
2021-10-11 21:20:18 +0300

    new BD Post in blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post (models.Model):
+    title=models.CharField(max_length=50, verbose_name='заголовок')
+    date=models.DateField(auto_now_add=True, verbose_name='дата')
+    text=models.TextField(verbose_name='текст поста')
+    image=models.ImageField(upload_to='image_event/', verbose_name='картинка')
+    
+    class Meta:
+        verbose_name_plural='посты'
+        verbose_name='Пост'
